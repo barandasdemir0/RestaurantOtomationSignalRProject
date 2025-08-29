45f5a05cc086e78197d4627de3ecfbce229f3b8b
Baran <barandasdemir.bd@gmail.com>
2025-08-29 18:47:03 +0300

    Ajax

---
+++ Frontend/Project.WebUI/Controllers/MenuController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Project.WebUI.Dtos.CategoryDtos;
+using Project.WebUI.Dtos.ProductDtos;
+using Project.WebUI.Models;
+
+namespace Project.WebUI.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MenuController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var pullData = await client.GetAsync("https://localhost:7240/api/Product/ProductListWithCategory");
+            var convertString = await pullData.Content.ReadAsStringAsync();
+            var convertData = JsonConvert.DeserializeObject<List<ResultProductDto>>(convertString);
+
+            //var client2 = _httpClientFactory.CreateClient();
+            //var pullData2 = await client2.GetAsync("https://localhost:7240/api/Category");
+            //var convertString2 = await pullData2.Content.ReadAsStringAsync();
+            //var convertData2 = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(convertString2);
+
+            //var viewModel = new ProductWithCategoryViewModel
+            //{
+            //    Categories = convertData2,
+            //    Products = convertData,
+            //};
+
+
+            return View(convertData);
+        }
+    }
+}
