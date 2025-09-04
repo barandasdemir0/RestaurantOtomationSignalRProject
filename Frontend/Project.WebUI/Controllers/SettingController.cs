eb3592997ee0ae9823be9432e53764feaace869f
Baran <barandasdemir.bd@gmail.com>
2025-09-04 19:31:32 +0300

    UserSetting

---
+++ Frontend/Project.WebUI/Controllers/SettingController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Project.EntityLayer.Concrete;
+using Project.WebUI.Dtos.IdentityDtos;
+using System.Threading.Tasks;
+
+namespace Project.WebUI.Controllers
+{
+    public class SettingController : Controller
+    {
+
+        private readonly UserManager<AppUser> _userManager;
+
+        public SettingController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity!.Name!);
+            UserEditDto userEditDto = new UserEditDto();
+            userEditDto.Surname = values.Surname;
+            userEditDto.Name = values.Name;
+            userEditDto.Mail = values.Email;
+            userEditDto.Username = values.UserName;
+            return View(userEditDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditDto userEditDto)
+        {
+            if (userEditDto.Password == userEditDto.ConfirmPassword)
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                user.Surname = userEditDto.Surname;
+                user.Name = userEditDto.Name;
+                user.Email = userEditDto.Mail;
+                user.UserName = userEditDto.Username;
+                user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, userEditDto.Password);
+                var result = await _userManager.UpdateAsync(user);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
